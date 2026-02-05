d52a61dbf40a517cc7b5b8abf478367423c2ec37
Jaydee White <145932487+PwurpleSlime@users.noreply.github.com>
2026-02-05 08:32:10 -0600

    Start Deleting the Proj to Start anew

--- src/connect4/connect4.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class Connect4Service {
-    public games: {id: string, gameState: number[][], playerOneTurn: boolean}[] = [];
-}
