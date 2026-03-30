e2ec8514313c91ecdd01c59fbe41f284fd99bdd1
Jaydee White <jddubble2@gmail.com>
2026-03-30 11:47:01 -0600

    Fixxed Rerouting Pathing

--- src/redirect/redirect.middleware.ts
+++ src/redirect/redirect.middleware.ts
@@ -14,13 +14,13 @@ export class RedirectMiddleware implements NestMiddleware {
 
     if (path === '/' || path === '' ) {
         console.log('No Base URL, Redirect to /v2');
-        return res.redirect(302, `/v2`)
+        return res.redirect(307, `/v2`)
     }
     const entry = ROUTE_VERSION_MAP.find(r => 
         path === r.prefix || path.startsWith(`${r.prefix}`)
     )
     const version = entry?.version ?? DEFAULT_VERSION
-    res.redirect(302, `/${version}${path}`)
+    res.redirect(307, `/${version}${path}`)
   }
 }
 
